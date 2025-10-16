aa689299862326863adf7bd83026ceefcbc7c9dc
MisterE1234 <MisterE1234@outlook.com>
2025-10-16 16:56:15 -0700

    4th Comit

--- 210-Midterm-1.cpp
+++ 210-Midterm-1.cpp
@@ -1,38 +1,40 @@
 //COMSC-210|Midterm 1| Ethan Aylard
 //IDE Used: Visual Studio Code
-// This Project
-#include <iostream>
-using namespace std;
+// This Project is the midterm exam for COMSC-210. I must comment all my code and create a new class method.
 
-const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20;
+#include <iostream> //Insterting the iostream library to allow for input and output to the console
+using namespace std; //Using the standard namespace to avoid having to type "std::" in front of everything.
 
-class DoublyLinkedList {
-private:
-// Node structure for doubly linked list that contains an integer data field and pointers to the previous and next nodes.:
-struct Node {
-int data;
-Node* prev;
-Node* next;
-Node(int val, Node* p = nullptr, Node* n = nullptr) {
-data = val;
-prev = p;
-next = n;
+const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20;// Declaring constant integers for easy modification later if needed.
+
+class DoublyLinkedList { //Creating a class, DoublyLinkedList, to represent a doubly linked list data structure.
+private: //Private access specifier to make it where only the class methods can access these members.
+
+struct Node {// Node structure for doubly linked list that contains an integer data field and pointers to the previous and next nodes.:
+int data; //declaring an integer variable to hold the data of the node
+Node* prev; //Pointer to the previous node in the list
+Node* next; //Pointer to the next node in the list
+
+Node(int val, Node* p = nullptr, Node* n = nullptr) { //Constructor to initialize a node with an integer value, previous pointer, and next pointer.
+data = val; // Set the data field to the provided value
+prev = p; // Set the previous pointer to the provided previous node
+next = n; // Set the next pointer to the provided next node
 }
 };
 
-Node* head;
-Node* tail;
+Node* head; //Pointer to the head (first node) of the list
+Node* tail; //Pointer to the tail (last node) of the list
 
-public:
-//Default Constructor:
-DoublyLinkedList() { head = nullptr; tail = nullptr; }
+public: //Public access specifier to make it these member are able to be accessed outside the class.
+
+DoublyLinkedList() { head = nullptr; tail = nullptr; } //Default Constructor: Initializes an empty list by setting head and tail to nullptr.
 
 //insert_after(): Insters a new value after a specified position in the list.
 //requires: and int for the value and an int for the position
 void insert_after(int value, int position) {
 // Making sure the position is not negative
-if (position < 0) {
-cout << "Position must be >= 0." << endl;
+if (position < 0) { // Position must be 0 or greater
+cout << "Position must be >= 0." << endl; //Output an error message if the position is negative
 return;
 }
 // Create the new node and making sure the list is not empty
@@ -209,31 +211,41 @@ delete temp;
 }
 }
 
-
+//print(): Prints the list from head to tail.
+//requires: nothing
 void print() {
+// Making sure the list is not empty
 Node* current = head;
 if (!current) {
 cout << "List is empty." << endl;
 return;
 }
+// Traverse the list and print each node's data
 while (current) {
 cout << current->data << " ";
 current = current->next;
 }
 cout << endl;
 }
+
+//print_reverse(): Prints the list from tail to head.
+//requires: nothing
 void print_reverse() {
+// Making sure the list is not empty
 Node* current = tail;
 if (!current) {
 cout << "List is empty." << endl;
 return;
 }
+// Traverse the list in reverse and print each node's data
 while (current) {
 cout << current->data << " ";
 current = current->prev;
 }
 cout << endl;
 }
+
+
 };
 int main() {
 cout << MIN_NR + MIN_LS + MAX_NR + MAX_LS; // dummy statement to avoid compiler warning
